4cb94d27192e7e90bd3a50af5f7206a75a984517
Jefferson Wu <jefferson.wu@180la.com>
2016-06-15 18:27:03 -0700

    latest and greatest, new spartan express server up and running

--- spartan-express-server.js
+++ spartan-express-server.js
@@ -15,6 +15,9 @@ var server = http.createServer(app);
 //COLORS!
 var colors = require('colors');
 
+//file system
+var fs = require('fs');
+
 /* ============= VARIABLES ============= */
 //global app info, should be populated based on file-read.
 var appInfo = {
@@ -26,6 +29,9 @@ var appInfo = {
         email: ''
     }
 };
+
+// port
+var port = process.argv[2];
 
 /* ============= MIDDLEWARE ============= */
 
@@ -39,14 +45,86 @@ app.use(function(req, res, next) {
 // log
 app.use(function(req, res, next){
     console.log('%s %s %s %s', req.method, req.url, req.path, colors.yellow( new Date().toString()));
+    next();
 });
 
-// 
-
+// static files
+app.use(express.static(__dirname + '/public/'));
 
 /* ============= ROUTES ============= */
 //default route
 app.get('/', function(req, res){
     res.type('text/html');
-    res.send('')
+    res.send('<h1>bipidy bopidy boo.</h1>');
 });
+
+//simple debug
+app.get('/debug/', function(req,res){
+    res.type('text/plain');
+    res.send('simple debug route working.');
+});
+
+//advanced debug
+app.get('/debug/:loc', function(req,res){
+    var inputMessage = req.params.loc;
+
+    res.type('text/html');
+    res.send('Debug route working. <h3> "' + inputMessage + '" </h3>');
+    console.log(req.params.loc);
+});
+
+//reads the package.json file
+app.get('/api/appdata', function(req, res){
+    fs.readFile(__dirname + '/package.json', function(err, data){
+        if(err) {
+            console.error(err);
+        } else {
+            //'data' object returns a buffer
+            //console.log(data.toString());
+            res.type('text/plain');
+            res.send(data.toString());
+            console.log('data sent');
+        }
+    });
+});
+
+
+
+//error route
+app.get('*', function(req, res){
+    res.sendFile(__dirname + '/public/404.html');
+});
+
+/* ============= RUNTIME ============= */
+grabApplicationInfo('package.json', appInfo);
+initServer(port);
+
+
+/* ============= FUNCTIONS ============= */
+function initServer(port) {
+    port = port || 3000;
+
+    app.listen(port, function(){
+        console.log(appInfo.appName + ' version ' + appInfo.version + ' running on port ' + port + '. ');
+    });
+}
+
+function grabApplicationInfo(filename, dataObject){
+    fs.readFile(__dirname + '/' + filename, function(err,data){
+        if(err){
+            console.error(err);
+        } else {
+            var jsonObject = JSON.parse(data);
+
+            console.log(colors.rainbow('app name: ') + jsonObject.name);
+            console.log(colors.rainbow('app version: ') + jsonObject.version);
+            console.log(colors.rainbow('private? ') + jsonObject.private);
+
+            console.log(colors.rainbow('dependencies: '));
+            for (var item in jsonObject.dependencies) {
+                console.log('   ' + item);
+            }
+            //console.log(jsonObject.dependencies);
+        }
+    });
+}
