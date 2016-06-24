2a66e36789edaf8c51f1a5407d6eda4d40d8d515
Jefferson Wu <jefferson.wu@180la.com>
2016-06-24 13:42:40 -0700

    using XHR2 and promises

---
+++ public/js/SoundBoard.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jefferson.wu on 6/24/16.
+ */
+var SoundBoard = (function () {
+    function SoundBoard() {
+    }
+    return SoundBoard;
+}());
+//# sourceMappingURL=SoundBoard.js.map
